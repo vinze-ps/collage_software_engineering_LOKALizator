9af8d0c5a217339bcceb97f3d818b6614a9ced3b
vinze-ps <patryk.surmacz12@gmail.com>
2024-05-23 19:32:40 +0200

    next

--- client/app/moje-konto/page.tsx
+++ client/app/moje-konto/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const MojeKontoPage = () => {
-  return <div className={"h-full w-full bg-background rounded-xl"}></div>;
+  return (
+    <div className={"h-full w-full bg-background rounded-xl flex gap-4"}>
+      <div className={""}></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default MojeKontoPage;

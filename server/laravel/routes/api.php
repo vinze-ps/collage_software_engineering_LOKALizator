e7d346b1995b8ef4629aa58a4290e20e5e78cbf0
Przemysław <przemo12397@gmail.com>
2024-06-02 09:03:39 +0200

    add /api/login endpoint

--- server/laravel/routes/api.php
+++ server/laravel/routes/api.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['Wprowadzone dane logowania są nieprawidłowe.'],
+        ]);
+    }
+
+    return $user->createToken()->plainTextToken;
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
